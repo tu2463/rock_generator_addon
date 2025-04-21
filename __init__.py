44a53d0b8042720124b43bb43d555448826b9599
tu2463 <117417955+tu2463@users.noreply.github.com>
2025-04-21 00:39:07 -0400

    added rock mesh type

--- __init__.py
+++ __init__.py
@@ -11,6 +11,31 @@ bl_info = {
 import bpy
 import os
 
+"""
+Rock Mesh Type
+"""
+
+class AddRockOperator(bpy.types.Operator):
+    bl_idname = "mesh.primitive_rock_add"
+    bl_label = "Rock"
+    bl_description = "Add a Rock with RockGenerator and RockMaterial"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.mesh.primitive_cube_add()
+        obj = context.active_object
+        if obj:
+            apply_geometry_nodes_modifier(obj)
+        return {'FINISHED'}
+
+def add_rock_to_mesh_menu(self, context):
+    self.layout.operator(AddRockOperator.bl_idname, text="Rock", icon='MESH_CUBE')
+
+
+"""
+Rock Generator, Rock Material
+"""
+
 ROCK_GROUP_NAME = "RockGenerator"
 ROCK_MATERIAL_NAME = "RockMaterial"
 ROCK_BLEND_FILE = "rock_generator.blend"
@@ -95,8 +120,10 @@ def register():
     bpy.app.timers.register(append_node_group, first_interval=0.1)
     bpy.app.handlers.load_post.append(on_file_load)
     bpy.app.handlers.depsgraph_update_post.append(on_scene_update)
-    print("[RockGen] Add-on registered.")
 
+    bpy.utils.register_class(AddRockOperator)
+    bpy.types.VIEW3D_MT_mesh_add.append(add_rock_to_mesh_menu)
+    print("[RockGen] Add-on registered.")
 
 def unregister():
     if on_file_load in bpy.app.handlers.load_post:
